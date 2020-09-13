6a1e90180a62a1d118947dea99f504cf32d1b840
leon <757365800@qq.com>
2020-09-13 17:16:46 +0800

    init

---
+++ CommonHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetPro.TypeFinder
+{
+    /// <summary>
+    /// Represents a common helper
+    /// </summary>
+    // ReSharper disable once PartialTypeWithSinglePart
+    public partial class CoreHelper
+    {
+        #region Fields
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the default file provider
+        /// </summary>
+        public static INetProFileProvider DefaultFileProvider { get; set; }
+
+        #endregion
+    }
+}
